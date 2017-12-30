80e225357a47873c39c70178366b8f79266f4573
Adam Jurík <ja004@seznam.cz>
2017-12-30 17:25:38 +0100

    Courier movement

---
+++ Assets/Courier.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Animations;
+using UnityEngine;
+
+public class Courier : MonoBehaviour
+{
+
+	[SerializeField] private float speed = 1;
+	[SerializeField] private float rotationSpeed = 1;
+	private Animator a;
+	public Courier otherCourier;
+
+
+	// Use this for initialization
+	void Start ()
+	{
+		a = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.W))
+		{
+			transform.position += -transform.forward * speed;
+			a.SetBool("walk", true);
+		}
+		else
+		{
+			a.SetBool("walk", false);
+		}
+
+		if(Input.GetKey(KeyCode.A))
+		{
+			transform.Rotate(Vector3.up, -rotationSpeed);
+		}
+		else if(Input.GetKey(KeyCode.D))
+		{
+			transform.Rotate(Vector3.up, rotationSpeed);
+		}
+	}
+
+	public void SwapCourier()
+	{
+		enabled = false;
+		otherCourier.enabled = true;
+	}
+}
---
+++ Assets/GameController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+	private Courier activeCourier;
+	[SerializeField]
+	private Courier normalCourier;
+	[SerializeField]
+	private Courier flyingCourier;
+
+	[SerializeField] private DragMouseOrbit camera;
+
+	// Use this for initialization
+	void Start ()
+	{
+		activeCourier = normalCourier;
+		normalCourier.otherCourier = flyingCourier;
+		flyingCourier.otherCourier = normalCourier;
+		camera.target = activeCourier.transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			SwapCourier();
+		}
+	}
+
+	private void SwapCourier()
+	{
+		activeCourier.SwapCourier();
+		activeCourier = normalCourier.enabled ? normalCourier : flyingCourier;
+		camera.target = activeCourier.transform;
+	}
+}
